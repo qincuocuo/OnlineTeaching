cf1d7ef070019728de9e622fa4fddd1ef1121910
YuSheng-qxx <1473460024@qq.com>
2022-05-20 18:01:49 +0800

    提交代码

--- common/models/learning_content.go
+++ common/models/learning_content.go
@@ -1,6 +1,14 @@
 package models
 
 type LearningContent struct {
-	Id          int64  `json:"id"`
-	Title       string `json:"title"` // 标题
+	Id            int64    `json:"id"`
+	Title         string   `json:"title"`          // 标题
+	FinishedNum   int      `json:"finished_num"`   //已学人数
+	UnfinishedNum int      `json:"unfinished_num"` //未学人数
+	Finished      []string `json:"finished"`       //已学学生id
+	Unfinished    []string `json:"unfinished"`     //未学学生id
+}
+
+func (l *LearningContent) CollectName() string {
+	return "tb_learning_content"
 }
--- common/models/register.go
+++ common/models/register.go
@@ -1,3 +1,15 @@
 package models
 
+// Register 签到表
+type Register struct {
+	RegisterId int      `json:"register_id"` //签到任务id
+	ManagerId  string   `json:"manager_id"`  //教师id
+	CourseId   int      `json:"course_id"`   //课程id
+	ContentId  int      `json:"content_id"`  //学习内容id
+	Finished   []string `json:"finished"`    //完成签到学生id
+	Unfinished []string `json:"unfinished"`  //未完成签到学生id
+}
 
+func (r *Register) CollectName() string {
+	return "tb_register"
+}
