772c58bcc619c0feb644463d9418490374de93b3
ouyujian <ouyujian@moresec.cn>
2022-05-26 21:39:00 +0800

    添加编译脚本

---
+++ webapi/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# shellcheck disable=SC2112
+function main() {
+  docker build -t sun:1.0 -f build/dockerfile/Dockerfile .
+  docker save sun:1.0 -o build/images/sun.tar
+  scp build/images/sun.tar guest@121.199.167.227:/home/guest/onlineTeaching/images
+}
+
+main
